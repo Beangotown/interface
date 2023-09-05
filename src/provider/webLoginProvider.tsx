a5133b5d8eeea9a8b5d83a4879042e8f0184a4a8
amber-bean <amber@beangotown.com>
2023-09-05 18:44:48 +0800

    feat: networkType

--- src/provider/webLoginProvider.tsx
+++ src/provider/webLoginProvider.tsx
@@ -6,5 +6,5 @@ import { NetworkType, PortkeyProvider } from '@portkey/did-ui-react';
 import { store } from 'redux/store';
 
 export default ({ children }: { children: React.ReactNode }) => {
-  return <PortkeyProvider networkType={'MAIN'}>{children}</PortkeyProvider>;
+  return <PortkeyProvider networkType={'TESTNET'}>{children}</PortkeyProvider>;
 };
