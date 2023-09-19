4deacd2fc2901e7f7c171c16a80aedbbdb96bdf2
amber-bean <amber@beangotown.com>
2023-09-19 20:35:43 +0800

    feat: update dev rewrites config

--- build.config/rewrites/development.js
+++ build.config/rewrites/development.js
@@ -1,27 +1,22 @@
 module.exports = [
   {
     source: '/connect/:path*',
-    destination: 'http://192.168.67.51:8080/connect/:path*',
+    destination: 'http://192.168.67.241:8080/connect/:path*',
   },
   {
     source: '/portkey/:path*',
-    destination: 'http://192.168.67.51:5577/:path*',
-    // destination: 'https://localtest-applesign2.portkey.finance/:path*',
+    destination: 'http://192.168.67.241:5577/:path*',
   },
   {
     source: '/api/:path*',
-    destination: 'http://192.168.66.205:5008/api/:path*',
+    destination: 'http://192.168.67.234:8100/api/:path*',
   },
   {
     source: '/cms/:path*',
-    destination: 'http://192.168.66.205:8100/:path*',
-  },
-  {
-    source: '/AElfIndexer_DApp/PortKeyIndexerCASchema/:path*',
-    destination: 'http://192.168.67.51:8083/AElfIndexer_DApp/PortKeyIndexerCASchema/:path*',
+    destination: 'http://192.168.67.234:8100/:path*',
   },
   {
     source: '/AElfIndexer_BeangoTown/BeangoTownIndexerPluginSchema/:path*',
-    destination: 'http://192.168.66.159:8095/AElfIndexer_BeangoTown/BeangoTownIndexerPluginSchema/:path*',
+    destination: 'http://192.168.67.234:8100/AElfIndexer_BeangoTown/BeangoTownIndexerPluginSchema/:path*',
   },
 ];
