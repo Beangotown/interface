86b67eff039de68297fca6279e9bca887b3237a6
amber-bean <amber@beangotown.com>
2023-10-09 13:33:53 +0800

    feat: add sentry captureMessage

--- src/utils/getTxResult.ts
+++ src/utils/getTxResult.ts
@@ -1,6 +1,7 @@
 import AElf from 'aelf-sdk';
 import { sleep } from './common';
 import { TargetErrorType } from './formattError';
+import { SentryMessageType, Severity, captureMessage } from './captureMessage';
 
 export function getAElf(rpcUrl?: string) {
   const rpc = rpcUrl || '';
@@ -21,10 +22,34 @@ export async function getTxResult(
 ): Promise<any> {
   const txResult = await getAElf(rpcUrl).chain.getTxResult(TransactionId);
   if (txResult.error && txResult.errorMessage) {
+    captureMessage({
+      type: SentryMessageType.HTTP,
+      params: {
+        name: 'getTxResult',
+        method: 'get',
+        query: {
+          TransactionId,
+          rpcUrl,
+        },
+        description: txResult,
+      },
+    });
     throw Error(txResult.errorMessage.message || txResult.errorMessage.Message);
   }
 
   if (!txResult) {
+    captureMessage({
+      type: SentryMessageType.HTTP,
+      params: {
+        name: 'getTxResult',
+        method: 'get',
+        query: {
+          TransactionId,
+          rpcUrl,
+        },
+        description: 'no txResult',
+      },
+    });
     throw Error(TargetErrorType.Default);
   }
 
@@ -37,22 +62,74 @@ export async function getTxResult(
   if (txResult.Status.toLowerCase() === 'notexisted' && reNotexistedCount) {
     await sleep(500);
     reNotexistedCount--;
+    captureMessage({
+      type: SentryMessageType.HTTP,
+      params: {
+        name: 'getTxResult',
+        method: 'get',
+        query: {
+          TransactionId,
+          rpcUrl,
+        },
+        description: {
+          txResult,
+          reNotexistedCount,
+        },
+      },
+      level: Severity.Info,
+    });
     return getTxResult(TransactionId, chainId, reGetCount, rpcUrl, reNotexistedCount);
   }
 
   if (txResult.Status.toLowerCase() === 'mined') {
     return { TransactionId, txResult };
   }
+  captureMessage({
+    type: SentryMessageType.HTTP,
+    params: {
+      name: 'getTxResult',
+      method: 'get',
+      query: {
+        TransactionId,
+        rpcUrl,
+      },
+      description: txResult,
+    },
+  });
   throw Error(TargetErrorType.Default);
 }
 
 export async function getTxResultOnce(TransactionId: string, rpcUrl: string): Promise<any> {
   const txResult = await getAElf(rpcUrl).chain.getTxResult(TransactionId);
   if (txResult.error && txResult.errorMessage) {
+    captureMessage({
+      type: SentryMessageType.HTTP,
+      params: {
+        name: 'getTxResultOnce',
+        method: 'get',
+        query: {
+          TransactionId,
+          rpcUrl,
+        },
+        description: txResult,
+      },
+    });
     throw Error(txResult.errorMessage.message || txResult.errorMessage.Message);
   }
 
   if (!txResult) {
+    captureMessage({
+      type: SentryMessageType.HTTP,
+      params: {
+        name: 'getTxResultOnce',
+        method: 'get',
+        query: {
+          TransactionId,
+          rpcUrl,
+        },
+        description: 'no txResult',
+      },
+    });
     throw Error(TargetErrorType.Default);
   }
 
@@ -65,6 +142,19 @@ export async function getTxResultOnce(TransactionId: string, rpcUrl: string): Pr
   }
 
   if (txResult.Status.toLowerCase() === 'notexisted') {
+    captureMessage({
+      type: SentryMessageType.HTTP,
+      params: {
+        name: 'getTxResultOnce',
+        method: 'get',
+        query: {
+          TransactionId,
+          rpcUrl,
+        },
+        description: txResult,
+      },
+      level: Severity.Info,
+    });
     return {
       status: 'notexisted',
       TransactionId,
@@ -76,5 +166,17 @@ export async function getTxResultOnce(TransactionId: string, rpcUrl: string): Pr
     return { TransactionId, txResult };
   }
 
+  captureMessage({
+    type: SentryMessageType.HTTP,
+    params: {
+      name: 'getTxResultOnce',
+      method: 'get',
+      query: {
+        TransactionId,
+        rpcUrl,
+      },
+      description: txResult,
+    },
+  });
   throw Error(TargetErrorType.Default);
 }
